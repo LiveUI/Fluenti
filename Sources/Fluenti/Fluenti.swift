0c839583d78655c190b1323548c34ace0fcc3e89
Ondrej Rafaj <ondrej.rafaj@gmail.com>
2018-06-17 00:48:59 +0100

    wip

--- Sources/Fluenti/Fluenti.swift
+++ Sources/Fluenti/Fluenti.swift
@@ -1,17 +1,3 @@
-//
-//  Fluenti.swift
-//  Fluenti
-//
-//  Created by Ondrej Rafaj on 16/06/2018.
-//  Copyright © 2018 LiveUI. All rights reserved.
-//
-
-import Foundation
-@_exported import FluentSQLite
-
-
-public final class Fluenti {
-    
-    // TODO: Create convenience methods to initialise the entire system for use on iOS
-    
+struct Fluenti {
+    var text = "Hello, World!"
 }
